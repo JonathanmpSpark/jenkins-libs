2007f1b336a1c337b6b81ec11af09a5d0ec96499
Jonathan <jonathan.munoz@sparktechs.com>
2022-12-05 12:59:17 -0800

    vars

--- vars/dockerDefs.groovy
+++ vars/dockerDefs.groovy
@@ -1,14 +1,14 @@
 def build(Map params){
-	ssh "date"
-	ssh 'date'
-	ssh 'Execute from dockerDefs. ${params.Name}'
-//	ssh "docker build -f ${params.Dockerfile} -t ${params.DockerImage} ${params.DockerContext}"
+	sh "date"
+	sh 'date'
+	sh 'Execute from dockerDefs. ${params.Name}'
+//	sh "docker build -f ${params.Dockerfile} -t ${params.DockerImage} ${params.DockerContext}"
 }
 
 def push(Map params){
-	ssh "docker push ${params.DockerImage}"
+	sh "docker push ${params.DockerImage}"
 }
 
 def test(Map params){
-	ssh 'echo "HELLO WORLD!!!"'
+	sh 'echo "HELLO WORLD!!! ${params.Name}"'
 }
