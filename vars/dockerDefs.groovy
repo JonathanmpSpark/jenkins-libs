fdc60d4e2f6937a5c7b8515c19bf08592e1df3f8
Jonathan <jonathan.munoz@sparktechs.com>
2022-12-05 13:35:30 -0800

    vars

--- vars/dockerDefs.groovy
+++ vars/dockerDefs.groovy
@@ -1,11 +1,11 @@
 def compose-build(Map params){
-	sh "IMAGE_TAG='${params.Tag}' docker-compose -f ${params.ComposeFile} build ${params.services}"
+	sh "IMAGE_TAG='${params.Tag}' docker-compose -f ${params.ComposeFile} build ${params.Service}"
 }
 
 def compose-up(Map params){
-	sh "IMAGE_TAG='${params.Tag}' docker-compose -f ${params.ComposeFile} up -d ${params.services}"
+	sh "IMAGE_TAG='${params.Tag}' docker-compose -f ${params.ComposeFile} up -d ${params.Service}"
 }
 
 def unittest(Map params){
-	sh "docker exec -i -u root ${params.service} bash -c pytest --html=/app/templates/pytest"
+	sh "docker exec -i -u root ${params.Service} bash -c pytest --html=/app/templates/pytest"
 }
